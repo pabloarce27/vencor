e1934d3a7270845184764b30a1a9bc39278bddeb
pablo <pabloleoarce@gmail.com>
2020-08-03 13:46:37 -0300

    botones en funcionamiento

--- menuAdmin.vb
+++ menuAdmin.vb
@@ -21,6 +21,7 @@
         End If
     End Sub
 
+#Region "botones"
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         showSubmenu(SubMenu1)
     End Sub
@@ -31,5 +32,44 @@
 
     Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
         showSubmenu(SubMenu3)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        abrirFormHijo(New prueba1())
+        hideSubmenu()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'codigo de accion'
+        hideSubmenu()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        'codigo de accion'
+        hideSubmenu()
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        abrirFormHijo(New prueba2())
+        hideSubmenu()
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        'codigo de accion'
+        hideSubmenu()
+    End Sub
+#End Region
+
+    Private currentForm As Form = Nothing
+    Private Sub abrirFormHijo(childForm As Form)
+        If currentForm IsNot Nothing Then currentForm.Close()
+        currentForm = childForm
+        childForm.TopLevel = False
+        childForm.FormBorderStyle = FormBorderStyle.None
+        childForm.Dock = DockStyle.Fill
+        PanelFormHijos.Controls.Add(childForm)
+        PanelFormHijos.Tag = childForm
+        childForm.BringToFront()
+        childForm.Show()
     End Sub
 End Class
